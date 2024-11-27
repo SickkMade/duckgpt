f0273363fe6047114ccbbc699aa4189dd2bc97ea
sickk <jaakmyr@gmail.com>
2024-11-27 12:46:22 -0800

    edited input placeholder

--- src/Textbox.jsx
+++ src/Textbox.jsx
@@ -4,7 +4,7 @@ import { FaArrowUp } from "react-icons/fa6";
 function textbox({HandleKeyDown, SendMessage, setUserInputValue, inputRef}) {
   return (
     <div className="textbox--wrapper">
-    <input ref={inputRef} type="text" className="textbox" placeholder='Message ChatGPT' onChange={(e) => setUserInputValue(e.target.value)} onKeyDown={HandleKeyDown}required/>
+    <input ref={inputRef} type="text" className="textbox" placeholder='Message DuckGPT' onChange={(e) => setUserInputValue(e.target.value)} onKeyDown={HandleKeyDown}required/>
     <div title="Press to send message" className="textbox--send" onClick={() => SendMessage(inputRef.current)}><FaArrowUp /></div>
     </div>
     
