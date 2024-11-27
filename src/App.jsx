7cdf2e73bac4435e38809379d45ec66dd6bc541e
sickk <jaakmyr@gmail.com>
2024-11-27 10:54:55 -0800

    duck gpt

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import Textbox from "./textbox"
+import { useState, useRef, useCallback } from "react"
+import Message from "./message";
+
+function App() {
+  const [isLongUi, setIsLongUi] = useState(false);
+  const [userInputValue, setUserInputValue] = useState('')
+  const [messages, setMessages] =  useState([]);
+  const [canSendMessage, setCanSendMessage] = useState(true);
+
+  const punctuation = ['!', '...', ',', '.', ' -', ' 🦆', 'y', 'ing']
+  
+  const inputRef = useRef(null)
+  const messageWrapperRef = useRef(null)
+
+  const SetLongUi = () => setIsLongUi(true);
+
+  const ScrollDown = useCallback(() => {
+    if (messageWrapperRef.current) {
+      messageWrapperRef.current.scrollTop = messageWrapperRef.current.scrollHeight;
+    }
+  }, []); 
+
+  const CreateRandomText = () => {
+    //random piece of text from 0 - 100 words
+    const length = Math.floor(Math.random() * 34 + 1)
+    let output = ""
+    for(let i = 0; i < length; i++){
+      output += " qu"
+      for(let i = 0; i < Math.random() * 10; i++){
+        output += "a"
+      }
+      output += "ck"
+      if(Math.random() < .35){ //1 in ? for punc
+        output += punctuation[punctuation.length * Math.random() | 0]
+      }
+    }
+    return output
+  }
+
+  const HandleKeyDown = (event) => {
+    if(event.key == 'Enter'){
+      SendMessage(inputRef.current)
+    }
+  }
+
+  const SendMessage = (inputbox) => {
+    if(!canSendMessage) return;
+    setCanSendMessage(false);
+    SetLongUi()
+
+    inputbox.value = ''
+    setMessages([...messages, {text: userInputValue, sender: "user"}])
+
+    setMessages((prev) => [...prev, {text: CreateRandomText(), sender: "ai"}])
+    setTimeout(() => {
+      setCanSendMessage(true)
+    }, 1000)
+  }
+
+  return (
+    <>
+    {/* <nav>nav</nav> */}
+    <section ref={messageWrapperRef} className="app--messages">
+      {messages.map((message, index) => {
+        return <Message key={index} ScrollDown={ScrollDown} text={message.text} sender={message.sender}/>
+      })}
+    </section>
+    
+    <div className={`flex flex-col justify-content-center app--textbox ${isLongUi ? 'app--textbox__bottom' : ''}`}>
+      <h1 className={isLongUi ? `invisible` : ``}>What can I help with?</h1>
+      <Textbox inputRef={inputRef} SendMessage={SendMessage} setUserInputValue={setUserInputValue} HandleKeyDown={HandleKeyDown}/>
+      {/* <div className={isLongUi ? `invisible` : ``} >tags</div> */}
+    </div>
+    <span className="app--bottom-text">By messaging ChatGPT, you agree to our <u>Terms</u> and have read our <u>Privacy Policy</u>.</span>
+    </>
+  )
+}
+
+export default App
---
+++ src/Textbox.jsx
@@ -0,0 +1,14 @@
+import './css/textbox.css'
+import { FaArrowUp } from "react-icons/fa6";
+
+function textbox({HandleKeyDown, SendMessage, setUserInputValue, inputRef}) {
+  return (
+    <div className="textbox--wrapper">
+    <input ref={inputRef} type="text" className="textbox" placeholder='Message ChatGPT' onChange={(e) => setUserInputValue(e.target.value)} onKeyDown={HandleKeyDown}required/>
+    <div title="Press to send message" className="textbox--send" onClick={() => SendMessage(inputRef.current)}><FaArrowUp /></div>
+    </div>
+    
+  )
+}
+
+export default textbox
